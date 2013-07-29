03f10ff11da93467e1ab6a3224fa32ba5cfbe4e8
yannack <yannack@gmail.com>
2013-07-29 09:03:54 +0200

    added miss count option to watchdog tool

--- powerUSB.py
+++ powerUSB.py
@@ -13,12 +13,15 @@ parser.add_option("-i", "--init", dest="init_watchdog", action="store_true",defa
 parser.add_option("-p", "--period",
                   action="store", type="int",dest="period", default=60,
                   help="watchdog period in seconds (both for init and while loop)")
+parser.add_option("-m", "--misses",
+                  action="store", type="int",dest="misses", default=2,
+                  help="number of watchdog heartbeats we are allowed to miss before power cycle")
 
 (options, args) = parser.parse_args()
 
 if options.init_watchdog:
     libPowerUSB.stop_watchdog()
-    libPowerUSB.start_watchdog(options.period)
+    libPowerUSB.start_watchdog(options.period,allowed_misses=options.misses)
 
 def handle_signals(signum=3,func=None):
     """
