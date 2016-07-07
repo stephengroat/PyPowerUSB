d586f1915bb7f642d14fb0586fd60be5e1518a64
Stephen <stephengroat@users.noreply.github.com>
2016-07-07 11:39:55 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -6,5 +6,8 @@ setup(name='python-powerusb',
       version='0.0.1',
       author='Qualcomm Technologies, Inc.',
       author_email='sgroat@qti.qualcomm.com',
-      packages=['powerusb']
+      packages=['powerusb'],
+      install_requires=[
+            'usb'
+      ]
      )
