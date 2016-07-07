3c74a4d871922a92ce67faa9d4d6e0dcc1099f69
Stephen <stephengroat@users.noreply.github.com>
2016-07-07 08:53:52 -0700

    turning commands into dict structure, not working

--- pwrusb/pwrusb.py
+++ pwrusb/pwrusb.py
@@ -1,79 +1,84 @@
 import usb
 
-class pyPwrUSB:
+
+class PyPwrUSB:
     """PowerUSB Control Class
 
     Example:
         import pwrusb
 
-        pwrusb=pwrusb.pyPwrUSB()		
-        pwrusb.set_ports(port1=True)		
+        pwrusb=pwrusb.pyPwrUSB()
+        pwrusb.set_ports(port1=True)
         pwrusb.set_ports(port1=False)
         print pwr.get_firmware()
-        
+
     """
+
     def __init__(self):
-        devPwrUsb=None
-        self.handle=None
-        busses=usb.busses()
+        devpwrusb = None
+        self.handle = None
+        busses = usb.busses()
         for bus in busses:
-            devices=bus.devices
+            devices = bus.devices
             for dev in devices:
                 if dev.idVendor == 1240 and dev.idProduct == 63:
-                    devPwrUsb = dev
-        if devPwrUsb is not None:
-            self.handle=devPwrUsb.open()
-            #try:
+                    devpwrusb = dev
+        if devpwrusb is not None:
+            self.handle = devpwrusb.open()
+            # try:
             #    self.handle.detachKernelDriver(0)
-            #except usb.USBError:
+            # except usb.USBError:
             #    print "handle already detached"
             self.handle.claimInterface(0)
         else:
             raise Exception('Failed to find PowerUSB device')
 
+    PowerUSB_DEVICE_ID = "Vid_04d8&Pid_003F"  # This is the Vendor ID and Product ID for PowerUSB microcontroller
 
-    PowerUSB_DEVICE_ID = "Vid_04d8&Pid_003F"            # This is the Vendor ID and Product ID for PowerUSB microcontroller
-
-    #BUF_LEN = 256
+    # BUF_LEN = 256
     BUF_WRT = 64
 
     # Commands to write to PowerUSB
-    ON_PORT1 = 'A'
-    OFF_PORT1 = 'B'
-    ON_PORT2 = 'C'
-    OFF_PORT2 = 'D'
-    ON_PORT3 = 'E'
-    OFF_PORT3 = 'P'
+    commands = {
+        'SET_PORT_ALL_ON': chr(0xa5),
+        'SET_PORT_ALL_OFF': chr(0xa6),
 
-    DEFON_PORT1 = 'N'
-    DEFOFF_PORT1 = 'F'
-    DEFON_PORT2 = 'G'
-    DEFOFF_PORT2 = 'Q'
-    DEFON_PORT3 = 'O'
-    DEFOFF_PORT3 = 'H'
+        'SET_PORT1_ON': 'A',
+        'SET_PORT1_OFF': 'B',
+        'SET_PORT2_ON': 'C',
+        'SET_PORT2_OFF': 'D',
+        'SET_PORT3_ON': 'E',
+        'SET_PORT3_OFF': 'P',
 
-    READ_P1 = chr(0xa1)
-    READ_P2 = chr(0xa2)
-    READ_P3 = chr(0xac)
+        'SET_PORT1_DEFAULT_ON': 'N',
+        'SET_PORT1_DEFAULT_OFF': 'F',
+        'SET_PORT2_DEFAULT_ON': 'G',
+        'SET_PORT2_DEFAULT_OFF': 'Q',
+        'SET_PORT3_DEFAULT_ON': 'O',
+        'SET_PORT4_DEFAULT_OFF': 'H',
 
-    READ_P1_PWRUP = chr(0xa3)
-    READ_P2_PWRUP = chr(0xa4)
-    READ_P3_PWRUP = chr(0xad)
+        'READ_P1': [chr(0xa1), bool],
+        'READ_P2': [chr(0xa2), bool],
+        'READ_P3': [bool, chr(0xac)],
 
-    READ_FIRMWARE_VER = chr(0xa7)
-    READ_MODEL = chr(0xaa)
+        'READ_P1_PWRUP': [chr(0xa3), bool],
+        'READ_P2_PWRUP': [chr(0xa4), bool],
+        'READ_P3_PWRUP': [chr(0xad), bool],
 
-    READ_CURRENT = chr(0xb1)
-    READ_CURRENT_CUM = chr(0xb2)
-    RESET_CURRENT_COUNT = chr(0xb3)
-    WRITE_OVERLOAD = chr(0xb4) #TODO: what is this for ?
-    READ_OVERLOAD = chr(0xb5) #TODO: what is this for ?
-    SET_CURRENT_RATIO = chr(0xb6) #TODO: what is this for ?
-    RESET_BOARD = chr(0xc1) #TODO: what is this for ?
-    SET_CURRENT_OFFSET = chr(0xc2) #TODO: what is this for ?
+        'READ_FIRMWARE_VER': [chr(0xa7), int, 2],
+        'READ_MODEL': [chr(0xaa), int, 1],
 
-    ALL_PORT_ON = chr(0xa5)
-    ALL_PORT_OFF = chr(0xa6)
+        'READ_CURRENT': [chr(0xb1), int, 2],
+        'READ_CURRENT_CUM': [chr(0xb2), int, 4],
+        'RESET_CURRENT_COUNT': chr(0xb3)
+    }
+
+    WRITE_OVERLOAD = chr(0xb4)  # TODO: what is this for ?
+    READ_OVERLOAD = chr(0xb5)  # TODO: what is this for ?
+    SET_CURRENT_RATIO = chr(0xb6)  # TODO: what is this for ?
+    RESET_BOARD = chr(0xc1)  # TODO: what is this for ?
+    SET_CURRENT_OFFSET = chr(0xc2)  # TODO: what is this for ?
+
     SET_MODE = chr(0xa8)
     READ_MODE = chr(0xa9)
 
@@ -90,108 +95,118 @@ class pyPwrUSB:
 
     # Watchdog
     START_WDT = chr(0x90)
-    STOP_WDT = chr(0x91)        
+    STOP_WDT = chr(0x91)
     POWER_CYCLE = chr(0x92)
-    READ_WDT = chr(0x93)    #-> return the status.
+    READ_WDT = chr(0x93)  # -> return the status.
     HEART_BEAT = chr(0x94)
     SHUTDOWN_OFFON = chr(0x95)
 
     # Models
-    POWERUSB_MODELS={
+    POWERUSB_MODELS = {
         0: None,
         1: "Basic",
         2: "Digital IO",
         3: "Watchdog",
         4: "Smart Pro"
-        }
-    WATCHDOG_STATUS={
+    }
+    WATCHDOG_STATUS = {
         0: "Not running",
         1: "Active",
         2: "Power cycling",
         3: "On but about to do a planned power off",
         4: "Off during planned power off, or not running right after a (planned) power off"
-        }
+    }
 
-    def send_msg(self,msg):
-        padding = chr(0xFF) * (self.BUF_WRT - len(msg)) #usually, 63 chars
-        self.handle.bulkWrite(0x01,msg+padding,200)
+    def _send_msg(self, msg):
+        padding = chr(0xFF) * (self.BUF_WRT - len(msg))  # usually, 63 chars
+        self.handle.bulkWrite(0x01, msg + padding, 200)
 
-    def _read_msg(self,msg,reply_len):
-        self.send_msg(msg)
-        return self.handle.bulkRead(0x81,64,200)[0:reply_len] #significant reply
+    def _read_msg(self, msg, reply_len):
+        self._send_msg(msg)
+        return self.handle.bulkRead(0x81, 64, 200)[0:reply_len]  # significant reply
 
-    def read_bool(self,msg):
-        thebool=self._read_msg(msg,1)[0]
-        return bool(thebool)
+    def _read_bool(self, msg):
+        return bool(self._read_msg(msg, 1)[0])
 
-    def get_ports(self):
-        return self.read_bool(self.READ_P1),self.read_bool(self.READ_P2),self.read_bool(self.READ_P3)
+    #TODO: Combine _read_ints and _read_int
+    def _read_ints(self, msg, length):
+        return self._read_msg(msg, length)
 
-    def get_ports_defaults(self):
-        return self.read_bool(self.READ_P1_PWRUP),self.read_bool(self.READ_P2_PWRUP),self.read_bool(self.READ_P3_PWRUP)
-
-    def read_ints(self,msg,length):
-        return self._read_msg(msg,length)
-
-    def read_int(self,msg,length):
-        theints=self._read_msg(msg,length)
-        offset=0
-        result=0
-        for theint in theints[::-1]:
-            result|= theint << offset
-            offset+=8
+    #TODO: Combine _read_ints and _read_int
+    def _read_int(self, msg, length):
+        vals = self._read_msg(msg, length)
+        offset = 0
+        result = 0
+        for val in vals[::-1]:
+            result |= val << offset
+            offset += 8
         return result
 
+    def _read(self, msg):
+        if msg not in self.commands:
+            raise Exception('Invalid message')
+        if self.commands[msg][1] == bool:
+            return self._read_bool(msg)
+        elif self.commands[msg][1] == int:
+            return self._read_int(msg, self.commands[msg][2])
+
+    def get_ports(self):
+        return self._read(self.commands['READ_P1']), self._read(self.commands['READ_P2']), self._read(self.commands['READ_P3'])
+
+    def get_ports_defaults(self):
+        return self._read(self.commands['READ_P1_PWRUP']), self._read(self.commands['READ_P2_PWRUP']),\
+               self._read(self.commands['READ_P3_PWRUP'])
+
     def get_firmware(self):
-        ints=self.read_ints(self.READ_FIRMWARE_VER,2)
-        return "%d.%d" % (ints[0],ints[1])
+        ints = self._read_ints(self.commands['READ_FIRMWARE_VER'], 2)
+        return "%d.%d" % (ints[0], ints[1])
 
     def get_model(self):
-        return self.POWERUSB_MODELS[self.read_int(self.READ_MODEL,1)]
+        return self.POWERUSB_MODELS[self._read(self.commands['READ_MODEL'])]
 
     def get_current(self):
-        return self.read_int(self.READ_CURRENT,2) #in milliamps
+        return self._read_int(self.commands['READ_CURRENT'], 2)  # in milliamps
 
     def get_total_current(self):
-        total_current = self.read_int(self.READ_CURRENT_CUM,4)
-        return total_current #in amps/minute
+        total_current = self._read_int(self.commands['READ_CURRENT_CUM'], 4)
+        return total_current  # in amps/minute
 
     def reset_total_current(self):
-        self.send_msg(self.RESET_CURRENT_COUNT)
+        self._send_msg(self.commands['RESET_CURRENT_COUNT'])
 
-    def start_watchdog(self,expected_interval,allowed_misses=2,offtime=2):
-        #expected_interval: seconds between heartbeats (int)
-        #allowed_misses: number of consecutive misses allowed 
-        #offtime: amount of time to switch off the computer outlet, in seconds (int)
-        for param in (expected_interval,allowed_misses,offtime):
+    def start_watchdog(self, expected_interval, allowed_misses=2, offtime=2):
+        # expected_interval: seconds between heartbeats (int)
+        # allowed_misses: number of consecutive misses allowed
+        # offtime: amount of time to switch off the computer outlet, in seconds (int)
+        for param in (expected_interval, allowed_misses, offtime):
             assert type(param) is int
             assert param >= 0
             assert param <= 255
         msg = self.START_WDT + chr(0) + chr(expected_interval) + chr(allowed_misses) + chr(offtime)
-        self.send_msg(msg)
+        self._send_msg(msg)
 
     def stop_watchdog(self):
-        self.send_msg(self.STOP_WDT)
+        self._send_msg(self.STOP_WDT)
 
     def get_watchdog_status(self):
-        status = self.read_int(self.READ_WDT,1)
-        return status,self.WATCHDOG_STATUS[status]
+        status = self._read_int(self.READ_WDT, 1)
+        return status, self.WATCHDOG_STATUS[status]
 
     def send_heartbeat(self):
-        send_msg(self.HEART_BEAT)
+        self._send_msg(self.HEART_BEAT)
 
-    def power_cycle_watchdog_port(self,time_off):
+    def power_cycle_watchdog_port(self, time_off):
         assert type(time_off) is int
         assert time_off >= 0
         assert time_off <= 255
         msg = self.POWER_CYCLE + chr(time_off)
-        self.send_msg(msg)
+        self._send_msg(msg)
 
-    def planned_poweroff(self,time_to_off,time_to_on):
-        #time_to_off: minutes before turning off the watchdog port, <=255 minutes
-        #time_to_on: minutes before turning that port back on <= 720minutes=12 hours. Very imprecise.
-        #TODO: check which ports this really aplies to
-        for param in (time_to_off,time_to_on):
+    def planned_poweroff(self, time_to_off, time_to_on):
+        # time_to_off: minutes before turning off the watchdog port, <=255 minutes
+        # time_to_on: minutes before turning that port back on <= 720minutes=12 hours. Very imprecise.
+        # TODO: check which ports this really aplies to
+        for param in (time_to_off, time_to_on):
             assert type(param) is int
             assert param >= 0
         assert time_to_off <= 720
@@ -199,41 +214,44 @@ class pyPwrUSB:
         time_on_1 = (time_to_on >> 8) & 0x00ff
         time_on_2 = time_to_on & 0x00ff
         msg = self.SHUTDOWN_OFFON + chr(time_to_off) + chr(time_on_1) + chr(time_on_2)
-        self.send_msg(msg)
+        self._send_msg(msg)
 
-    def set_ports(self, port1=None,port2=None,port3=None):
-        msgs=[]
+    def set_ports(self, defaults=False, port1=None, port2=None, port3=None):
+        msgs = []
         if port1 is True:
-            msgs.append(self.ON_PORT1) #turn on port 1
-        if port1 is False:
-            msgs.append(self.OFF_PORT1) #turn off port 1
-        if port2 is True:
-            msgs.append(self.ON_PORT2) #turn on port 2
-        if port2 is False:
-            msgs.append(self.OFF_PORT2) #turn off port 2
-        if port3 is True:
-            msgs.append(self.ON_PORT3) #turn on port 3
-        if port3 is False:
-            msgs.append(self.OFF_PORT3) #turn off port 3
-        for msg in msgs:
-            self.send_msg(msg)
+            if defaults:
+                msgs.append(self.DEFON_PORT1)  # turn on port 1
+            msgs.append(self.commands[])  # turn on port 1
+        elif port1 is False:
 
-    def set_ports_defaults(self,port1=None,port2=None,port3=None):
-        msgs=[]
-        if port1 is True:
-            msgs.append(self.DEFON_PORT1) #turn on port 1
-        if port1 is False:
-            msgs.append(self.DEFOFF_PORT1) #turn off port 1
+            msgs.append(self.OFF_PORT1)  # turn off port 1
         if port2 is True:
-            msgs.append(self.DEFON_PORT2) #turn on port 2
-        if port2 is False:
-            msgs.append(self.DEFOFF_PORT2) #turn off port 2
+            msgs.append(self.ON_PORT2)  # turn on port 2
+        elif port2 is False:
+            msgs.append(self.OFF_PORT2)  # turn off port 2
         if port3 is True:
-            msgs.append(self.DEFON_PORT3) #turn on port 3
-        if port3 is False:
-            msgs.append(self.DEFOFF_PORT3) #turn off port 3
+            msgs.append(self.ON_PORT3)  # turn on port 3
+        elif port3 is False:
+            msgs.append(self.OFF_PORT3)  # turn off port 3
         for msg in msgs:
-            self.send_msg(msg)
+            self._send_msg(msg)
+
+    def set_ports_defaults(self, port1=None, port2=None, port3=None):
+        msgs = []
+        if port1 is True:
+            msgs.append(self.DEFON_PORT1)  # turn on port 1
+        elif port1 is False:
+            msgs.append(self.DEFOFF_PORT1)  # turn off port 1
+        if port2 is True:
+            msgs.append(self.DEFON_PORT2)  # turn on port 2
+        elif port2 is False:
+            msgs.append(self.DEFOFF_PORT2)  # turn off port 2
+        if port3 is True:
+            msgs.append(self.DEFON_PORT3)  # turn on port 3
+        elif port3 is False:
+            msgs.append(self.DEFOFF_PORT3)  # turn off port 3
+        for msg in msgs:
+            self._send_msg(msg)
 
     def __del__(self):
         self.handle.releaseInterface()
